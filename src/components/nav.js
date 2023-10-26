04b6092857982dbc7e8553480eb1d01266ac4655
Taylor Cox <taylorashtoncox@gmail.com>
2023-10-26 13:59:52 -0400

    create new portfolio with updates to environment

---
+++ src/components/nav.js
@@ -0,0 +1,33 @@
+import * as React from "react";
+import "../styles/styles.scss";
+import { scroller } from 'react-scroll';
+import Resume from "../images/Taylor-Dunworth-Resume.pdf";
+
+const Nav = () => {
+  const scrollToElement = (location) => {
+    scroller.scrollTo(location, {
+      duration: 800,
+      delay: 0,
+      smooth: 'easeInOutQuart'
+    });
+  };
+
+  return (
+    <section id="navigation">
+      <div>
+        <span className="logo">Taylor Dunworth</span>
+        <nav aria-label="Main Navigation">
+          <ul>
+            <li><button to="about" role="link" onClick={() => scrollToElement("about")}>About</button></li>
+            <li><a href="https://taylordunworth.myportfolio.com/">Work</a></li>
+            <li><a href={Resume} target="_blank" rel="noreferrer">Resume</a></li>
+            <li><button to="community" role="link" onClick={() => scrollToElement("community")}>Community</button></li>
+            <li><a href="mailto:taydunworth@gmail.com">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+    </section>
+  )
+}
+
+export default Nav;
